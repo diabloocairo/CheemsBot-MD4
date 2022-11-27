d813e88931f58f956aaab244bf452dae952dcdb6
diabloocairo <119153395+diabloocairo@users.noreply.github.com>
2022-11-27 12:46:11 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['9660540637980'] //ur owner number
-global.ownername = "ABDUL RAHMAN🐼🍷 ٫" //ur owner name
-global.ytname = "YT: ABDUL RAHMAN" //ur yt chanel name
-global.socialm = "GitHub: ABDUL RAHMAN" //ur github or insta name
+global.vcardowner = ['201114167089'] //ur owner number
+global.ownername = "Đᴅ Ꭵ Λ Ⲃ Ł O  Ꮯ Λ Ꭵ Ŕ O " //ur owner name
+global.ytname = "YT: Đᴅ Ꭵ Λ Ⲃ Ł O Ꮯ Λ Ꭵ Ŕ O " //ur yt chanel name
+global.socialm = "insta: db15_6 " //ur github or insta name
 global.location = "Egypt, Cairo, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['9660540637980']
-global.ownertag = '9660540637980' //ur tag number
-global.botname = 'BOT ABDUL RAHMAN🐼🍷 ٫' //ur bot name
+global.owner = ['201114167089']
+global.ownertag = '201114167089' //ur tag number
+global.botname = 'BOT Đᴅ Ꭵ Λ Ⲃ Ł O  Ꮯ Λ Ꭵ Ŕ O  ٫' //ur bot name
 global.linkz = "https://chat.whatsapp.com/LXZnV3rPFqD4ryPvG0Cyvvv" //your theme url which will be displayed on whatsapp
 global.websitex = "https://chat.whatsapp.com/LXZnV3rPFqD4ryPvG0Cyvvv" //ur website to be displayed
 global.botscript = 'https://chat.whatsapp.com/LXZnV3rPFqD4ryPvG0Cyvvv' //script link
 global.reactmoji = "🌚" //ur menu react emoji
 global.themeemoji = "🌞" //ur theme emoji
-global.packname = "⍣عبدالرحمن⍣" //ur sticker watermark packname
-global.author = "⍣9660540637980⍣" //ur sticker watermark author
+global.packname ="Đᴅ Ꭵ Λ Ⲃ Ł O  Ꮯ Λ Ꭵ Ŕ O " //ur sticker watermark packname
+global.author = "201114167089" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['9660540637980'] //ur premium numbers
+global.premium = ['201114167089'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -102,7 +102,7 @@ global.ntilink = []
 //omther
 global.sessionName = 'session'
 global.antitags = true
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.'🙂'💟','✨','🥰']
 global.sp = '⭔'
 global.mess = {
     success: 'تم يروحي ✓',
